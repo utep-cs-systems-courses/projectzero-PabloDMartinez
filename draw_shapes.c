376ff18e388b9700007a699b7dac9eb9a88d999a
PabloDMartinez <pdmartinez3@miners.utep.edu>
2023-09-13 16:50:21 -0600

    added arrows

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,9 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+void print_arrow(int leftCol, int size)
+{
+  int squareStart = leftCol+(size/2);
+  print_triangle(leftCol, size);
+  print_square(squareStart, size);
+}
